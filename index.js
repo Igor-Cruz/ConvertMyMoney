c2a38e71347de88f26290e03b140afd040bf0b8f
IGOR CRUZ <igorcruz.it@outlook.com>
2021-12-02 17:57:15 -0300

    Primeira versão do Covert My Money

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+const convert = require('./lib/convert')
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
@@ -9,12 +11,27 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
   res.render('home')
 })
-
-app.listen(3000, err => {
-  if (err) {
-    console.log('Unable to start')
+app.get('/cotacao', (req, res) => {
+  const { cotacao, quantidade } = req.query
+  if (cotacao && quantidade) {
+    const conversao = convert.convert(cotacao, quantidade)
+    res.render('cotacao', {
+      error: false,
+      cotacao: convert.toMoney(cotacao),
+      quantidade: convert.toMoney(quantidade),
+      conversao: convert.toMoney(conversao)
+    })
   } else {
-    console.log('ConvertMyMoney is online')
+    res.render('cotacao', {
+      error: 'Valores inválidos'
+    })
   }
 })
 
+app.listen(3000, err => {
+  if (err) {
+    console.log('Error não foi possível iniciar o servidor')
+  } else {
+    console.log('Tudo okay')
+  }
+})
