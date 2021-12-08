f6344bd9e406d42b5735e000dcc46fae2790b486
IGOR CRUZ <igorcruz.it@outlook.com>
2021-12-07 22:51:26 -0300

    Tratando erros

--- teste-api.js
+++ teste-api.js
@@ -1,7 +1,8 @@
 const axios = require('axios')
 
-const url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%2712-07-2021%27&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao'
+const url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%2712-07-2021%27&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao====='
 
 axios
   .get(url)
   .then(res => console.log(res.data.value[0].cotacaoVenda))
+  .catch(err => console.log(err))
